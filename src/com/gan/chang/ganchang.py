3044da7a00a6eae06a1226365adb6c8ea3c1d718
wuhaifengdhu <wuhaifengdhu@163.com>
2018-04-08 23:00:38 +0800

    fixed mark_char

--- src/com/gan/chang/ganchang.py
+++ src/com/gan/chang/ganchang.py
@@ -1,7 +1,7 @@
 import math
 def print_hourglass(total_number, mark_char):
         n = int(math.sqrt((total_number + 1)/2.0))
-        print '\n'.join([ ' ' * x + '*' * y for (x, y) in zip(range(0, n) + range(n-2, -1, -1), range(2*n -1, 1, -2) + range(1, 2*n, 2))])
+        print '\n'.join([ ' ' * x + mark_char * y for (x, y) in zip(range(0, n) + range(n-2, -1, -1), range(2*n -1, 1, -2) + range(1, 2*n, 2))])
         print total_number - 2 * n * n + 1
 
 
